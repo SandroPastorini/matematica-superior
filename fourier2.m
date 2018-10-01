c28c456288557ff8c3631e59a3df6fc874da0f55
Sandro Pastorini <sandropastorini@gmail.com>
2018-10-01 16:52:42 -0300

    primer commit. Actualmente trabajando en disipador(), agregada la matriz A del SE

---
+++ fourier2.m
@@ -0,0 +1,4 @@
+function [ak bk] = fourier2(f, a, b, error)
+  % tengo una funcion, el intervalo [a, b] y un error. Esto me tiene que dar la integral entre a y b de f(x)
+  I = int(f(x), a, b)
+endfunction
